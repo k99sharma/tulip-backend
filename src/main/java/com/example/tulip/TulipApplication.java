1edc77539a93738c49950adf19f7940b32abf8c8
Kalash Sharma <kalash.strt@gmail.com>
2024-03-10 00:38:31 +0530

    add: spring boot app created using spring initializr

---
+++ src/main/java/com/example/tulip/TulipApplication.java
@@ -0,0 +1,13 @@
+package com.example.tulip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TulipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TulipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tulip/TulipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tulip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TulipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
